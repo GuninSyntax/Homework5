396a2cff072f64ba58d4ac12d64780bd824a3431
Dmitry <gunin.syntax@gmail.com>
2023-11-12 21:54:08 +0400

    Lottery corrected

--- src/main/java/main_tasks/Lottery/Lottery.java
+++ src/main/java/main_tasks/Lottery/Lottery.java
@@ -1,34 +1,30 @@
 package main_tasks.Lottery;
+
 import java.util.Random;
 
 public class Lottery {
     public static void main(String[] args) {
-        int[] numbersOfLottery = mixingNumbers(initNumbersLottery());
-        printFirstNumbers(numbersOfLottery);
-    }
-
-    static int[] initNumbersLottery() {
-        int[] numbersOfLottery = new int[100];
-        for (int i = 0; i < numbersOfLottery.length; i++) {
-            numbersOfLottery[i] = i;
-        }
-        return numbersOfLottery;
-    }
-
-    static int[] mixingNumbers(int[] numbersOfLottery) {
+        int[] numbers = new int[100];
         Random random = new Random();
-        for (int i = 0; i < numbersOfLottery.length; i++) {
-            int value = random.nextInt(100);
-            int temp = numbersOfLottery[value];
-            numbersOfLottery[value] = numbersOfLottery[i];
-            numbersOfLottery[i] = temp;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
         }
-        return numbersOfLottery;
-    }
-
-    static void printFirstNumbers(int[] numbersOfLottery) {
+        for (int i = 0; i < numbers.length/2; i++) {
+            int randomNumber = random.nextInt(100);
+            int temp = numbers[i];
+            numbers[i] = numbers[randomNumber];
+            numbers[randomNumber] = temp;
+        }
         for (int i = 0; i < 10; i++) {
-            System.out.print(numbersOfLottery[i] + " ");
+            System.out.print(numbers[i] + " ");
         }
+        boolean duplicate = true;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[j] == numbers[i] && i != j) duplicate = false;
+            }
+        }
+        if (!duplicate) System.out.println("\nВ МАССИВЕ ЕСТЬ ДУБЛИКАТЫ");
+        else System.out.println("\nДУБЛИКАТОВ НЕТ");
     }
 }
